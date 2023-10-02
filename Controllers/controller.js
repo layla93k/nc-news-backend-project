491cefc76b54bb0f3758b141feb40c7ad7c2d393
Layla <layla.kawafi@hotmail.com>
2023-10-02 14:00:34 +0100

    this commit will show kata 3 completed

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,11 +1,21 @@
-const {fetchAllTopics} = require('../Models/models.js')
+const { fetchAllTopics, fetchAllEndpoints } = require('../Models/models.js')
 
 
 exports.getAllTopics = (req, res, next) => {
     fetchAllTopics().then((topics) => {
-        res.status(200).send(topics)
-    }).catch((err)=>{
-            console.log(err)
-            next(err)
+        res.status(200).send({ topics: topics })
+    }).catch((err) => {
+        console.log(err)
+        next(err)
+    })
+}
+
+
+exports.getAllEndpoints = (req, res, next) => {
+    fetchAllEndpoints().then((endpoints) => {
+        res.status(200).send({ availableEndpoints: endpoints })
+    }).catch((err) => {
+        console.log(err)
+        next(err)
     })
 }
