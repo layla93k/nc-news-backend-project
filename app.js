601de3d19057d7a8b8bb61490635369e18dadf4f
Layla <layla.kawafi@hotmail.com>
2023-10-23 12:14:11 +0100

    fixing CORS error

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const express = require("express");
-const data = require('./db/data/test-data/index.js')
-const { handleCustomErrors, handleSQLErrors } = require('./Error handling/error-handling.js');
+const data = require("./db/data/test-data/index.js");
+const {
+  handleCustomErrors,
+  handleSQLErrors,
+} = require("./Error handling/error-handling.js");
 
 //routers
-const articleRouter = require('./routes/article-router.js')
+const articleRouter = require("./routes/article-router.js");
 const commentsRouter = require("./routes/comments-router.js");
 const topicsRouter = require("./routes/topics-router.js");
 const usersRouter = require("./routes/users-router.js");
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+app.use("/api", articleRouter);
+app.use("/api", commentsRouter);
+app.use("/api", topicsRouter);
+app.use("/api", usersRouter);
 
-app.use('/api', articleRouter)
-app.use('/api', commentsRouter)
-app.use('/api', topicsRouter)
-app.use('/api', usersRouter)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 //error handling
-app.use(handleCustomErrors)
-app.use(handleSQLErrors)
-app.all('/*', (req, res, next) => {
-    res.status(404).send({ msg: 'Not found' })
-
-})
+app.use(handleCustomErrors);
+app.use(handleSQLErrors);
+app.all("/*", (req, res, next) => {
+  res.status(404).send({ msg: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ msg: 'Internal server error' })
-})
+  res.status(500).send({ msg: "Internal server error" });
+});
 module.exports = app;
